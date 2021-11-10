b15e6c89484df3ff8ef0007fc4fab3769739f5aa
HieuTrong123 <87361973+HieuTrong123@users.noreply.github.com>
2021-11-10 11:32:56 +0700

    super array

---
+++ Project1/Source.cpp
@@ -0,0 +1,9 @@
+#include"thuvien.h"
+int main() {
+	int *a = NULL;
+	int k = 0;
+	XuatMenu(a ,k);
+	delete[] a;
+	system("pause");
+	return 0;
+}
---
+++ Project1/thuvien.h
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+void Menu();
+void XuatMenu(int* a,int k);
+void ThemMotPhanTu(int*& a, int& n/*,int vt,int x*/);
