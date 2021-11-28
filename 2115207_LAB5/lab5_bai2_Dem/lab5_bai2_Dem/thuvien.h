310bf6bffffce7fea119a17ce6ecdeab8535e42b
HieuTrong123 <87361973+HieuTrong123@users.noreply.github.com>
2021-11-28 16:22:45 +0700

    BT lab

--- 2115207_LAB5/lab5_bai2_Dem/lab5_bai2_Dem/thuvien.h
+++ 2115207_LAB5/lab5_bai2_Dem/lab5_bai2_Dem/thuvien.h
@@ -6,6 +6,7 @@ int DemSoNgoaiMinMax(int a[], int n, int min, int max);
 int DemX(int a[], int n, int vt, int x);
 int DemSoChinhPhuong(int a[], int n);
 bool KiemTraSoChinhPhuong(int n);
+void DemSoDuongChay(int a[], int n);
 void NhapMang(int a[] ,int n) {
 	for (int i = 0; i < n;i++) {
 		cout << "\nnhap gia tri a[" << i << "] : ";
@@ -60,4 +61,34 @@ int DemX(int a[],int n,int vt,int x){
 		}
 	}
 	return dem;
+}
+void DemSoDuongChay(int a[],int n) {
+	int tang, giam;
+	int max[MAX], min[MAX];
+	int k1 = 0, k2 = 0;
+	for (int i = 0; i < n-1;i++) {
+		if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+			max[k1] = i;
+			k1++;
+		}
+		else if (a[i] < a[i - 1] && a[i] < a[i + 1]) {
+			min[k2] = i;
+			k2++;
+		}
+	}
+	if (a[n - 1] > a[n - 2])
+	{
+		max[k1] = n - 1;
+		k1++;
+	}
+	else {
+		min[k2] = n - 1;
+		k2++;
+	}
+	tang = k1;
+	giam = k2;
+	
+	cout << "\nso cac duong chay trong day la : " << tang+giam;
+	cout << "\nso duong chay tang dan la : " << tang;
+	cout << "\nso duong chay giam la : " << giam;
 }
